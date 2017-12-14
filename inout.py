88df0dcd9ae633775242ce9fbeebd3635c126d20
Katherine Young <kayoung@mit.edu>
2017-12-14 16:57:00 -0500

    Update inout.py

--- inout.py
+++ inout.py
@@ -2,6 +2,11 @@ import sys
 import gzip
 import random
 import numpy as np
+import tqdm
+import torch.utils.data as data
+
+PATH="../data/askubuntu-master/text_tokenized.txt.gz"
+DATASET_SIZE = 800
 
 # text_tokenized.txt.gz
 # maps query IDs to their title and body, body is a list of words
@@ -15,6 +20,36 @@ def read_corpus(path):
             body = body.strip().split()
             raw_corpus[query_id] = (title, body)
     return raw_corpus
+
+def getEmbeddingTensor():
+
+    embedding_path='../data/askubuntu-master/vector/vectors_pruned.200.txt.gz'
+    lines = []
+    with gzip.open(embedding_path) as file:
+        lines = file.readlines()
+        file.close()
+    embedding_tensor = []
+    word_to_indx = {}
+    for indx, l in enumerate(lines):
+        word, emb = l.split()[0], l.split()[1:]
+        vector = [float(x) for x in emb ]
+        if indx == 0:
+            embedding_tensor.append( np.zeros( len(vector) ) )
+        embedding_tensor.append(vector)
+        word_to_indx[word] = indx+1
+    embedding_tensor.append(np.zeros(len(vector)))
+    embedding_tensor = np.array(embedding_tensor, dtype=np.float32)
+    return embedding_tensor, word_to_indx
+
+def load_dataset(args):
+    print("\nLoading data...")
+    embeddings, word_to_indx = getEmbeddingTensor()
+    args.embedding_dim = embeddings.shape[1]
+
+    train_data = dataset.Dataset('train', word_to_indx)
+    dev_data = dataset.Dataset('heldout', word_to_indx)
+
+    return train_data, dev_data, embeddings
 
 # vectors_pruned.200.txt.gz
 # Maps a word to 200-dimension (1D array) feature vector
@@ -93,7 +128,7 @@ def create_samples(id_samples, word_embs, raw_corpus):
     		title_sample.append(line2vec(title, word_embs))
     		body_sample.append(line2vec(body, word_embs))
     	all_samples.append((title_sample, body_sample))
-    return all_samples
+    return np.array(all_samples)
 
 # Turns list of train samples into batches
 # concatenates all lists within each batch
@@ -177,3 +212,51 @@ def create_dev_test_data(samples, labs, word_embs, raw_corpus):
 		labels.extend(labs[i])
 
 	return ([title_data, body_data], labels)
+
+
+
+
+class Dataset(data.Dataset):
+
+    def __init__(self, name, word_to_indx, max_length=50, stem="../data/askubuntu-master/text_tokenized"):
+        self.name = name
+        self.dataset = []
+        self.word_to_indx  = word_to_indx
+        self.max_length = max_length
+        self.raw_corpus = inout.read_corpus(PATH)
+
+        with gzip.open(PATH.format(name)) as gfile:
+            lines = gfile.readlines()[:DATASET_SIZE]
+            for line in tqdm.tqdm(lines):
+                sample = self.processLine(line)
+                self.dataset.append(sample)
+            gfile.close()
+
+
+    ## EDIT THIS PART
+    def processLine(self, line):
+        labels = [ float(v) for v in line.split()[:5] ]
+
+        label = float(labels[0])
+        text = line.split('\t')[-1].split()[:self.max_length]
+        x =  getIndicesTensor(text, self.word_to_indx, self.max_length)
+        sample = {'x':x, 'y':label}
+        return sample
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self,index):
+        sample = self.dataset[index]
+        return sample
+
+
+def getIndicesTensor(text_arr, word_to_indx, max_length):
+    unk_indx = 0
+    text_indx = [ word_to_indx[x] if x in word_to_indx else unk_indx for x in text_arr][:max_length]
+    if len(text_indx) < max_length:
+        text_indx.extend( [nil_indx for _ in range(max_length - len(text_indx))])
+
+    x =  torch.LongTensor(text_indx)
+
+    return x
